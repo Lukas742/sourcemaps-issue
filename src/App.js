e8097c230888badd8d272895a9def7371c7c14e9
Lukas Harbarth <lukas.harbarth@sap.com>
2022-08-09 12:06:46 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "@ui5/webcomponents-fiori/dist/BarcodeScannerDialog.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ui5-barcode-scanner-dialog></ui5-barcode-scanner-dialog>
     </div>
   );
 }
